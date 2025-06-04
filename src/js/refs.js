c214410fc4753ff0070c5674dbe6acd12beedf3f
NikolayMiroshnichenkoUK <myroshnychenko.mykola@flawless.group>
2025-06-04 18:40:43 +0300

    first commmit

--- src/js/refs.js
+++
@@ -1,8 +0,0 @@
-const refs = {
-    taskList: document.querySelector('#task-list'),
-    taskForm: document.querySelector('#task-form'),
-    themeBtn: document.getElementById('themeToggle'),
-    body: document.querySelector('body')
-}
-
-export default refs;
