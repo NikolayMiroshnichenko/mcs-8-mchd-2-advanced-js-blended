09d4ee39837c524366a4cb1881f7d937eb7f12fd
NikolayMiroshnichenkoUK <myroshnychenko.mykola@flawless.group>
2025-06-04 19:41:53 +0300

    fix

--- src/main.js
+++ src/main.js
@@ -1,4 +1,15 @@
-
-
-
 const AUTH_TOKEN = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiZDU5NjBkZmE3YmY3ODdkYTU5NzdjMzA4NzkyNjcxOCIsIm5iZiI6MTYwMzQ4NTg3NC4wNDgsInN1YiI6IjVmOTM0MGIyN2ViNWYyMDA1YTAwY2ZhNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.-35wsiUmFgUrlSH8Ef-TWv01vcbpp5ysK7oX-BURKOM'
+
+
+function init() {
+  fetch(`https://api.themoviedb.org/3/trending/all/week`, {
+    method: 'GET',
+    headers: {
+      'Authorization': `Bearer ${AUTH_TOKEN}`,
+      'Accept': 'application/json',
+      'Content-Type': 'application/json;charset=utf-8'
+    }
+  }).then(res => res.json()).then(data => console.log('data', data))
+}
+
+init();
