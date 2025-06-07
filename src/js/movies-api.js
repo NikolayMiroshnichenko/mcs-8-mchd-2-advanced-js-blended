becc740c796583b71e85557364e547634f6611be
NikolayMiroshnichenkoUK <myroshnychenko.mykola@flawless.group>
2025-06-07 12:50:50 +0300

    fix

--- src/js/movies-api.js
+++ src/js/movies-api.js
@@ -1,35 +1,9 @@
 export const AUTH_TOKEN = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiZDU5NjBkZmE3YmY3ODdkYTU5NzdjMzA4NzkyNjcxOCIsIm5iZiI6MTYwMzQ4NTg3NC4wNDgsInN1YiI6IjVmOTM0MGIyN2ViNWYyMDA1YTAwY2ZhNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.-35wsiUmFgUrlSH8Ef-TWv01vcbpp5ysK7oX-BURKOM';
 
-export const getTrendingMovies = async (page) => {
-    try {
-        const response = await fetch(`https://api.themoviedb.org/3/trending/all/week?page=${page}`, {
-            method: 'GET',
-            headers: {
-                'Authorization': `Bearer ${AUTH_TOKEN}`,
-                'Accept': 'application/json',
-                'Content-Type': 'application/json;charset=utf-8'
-            }
-        });
+export const getTrendingMovies = async () => {
 
-        return response.json();
-    } catch {
-        return false;
-    }
 };
 
-export const getSearchMovies = async (query = '', page) => {
-    try {
-        const response = await fetch(`https://api.themoviedb.org/3/search/movie?query=${query}&page=${page}`, {
-            method: 'GET',
-            headers: {
-                'Authorization': `Bearer ${AUTH_TOKEN}`,
-                'Accept': 'application/json',
-                'Content-Type': 'application/json;charset=utf-8'
-            }
-        });
+export const getSearchMovies = async () => {
 
-        return response.json();
-    } catch {
-        return false;
-    }
 };
--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -1,34 +1,19 @@
-import { refs } from './refs';
+export const createMoviesMarkup = () => {
+    // <li class="movie-item">
+    //     <img
+    //         class="movie-poster"
+    //         src="https://image.tmdb.org/t/p/w300/"
+    //         alt=""
+    //         loading="lazy"
+    //     />
+    //     <div class="movie-info">
+    //         <div>
+    //             <h3 class="movie-title"></h3>
+    //             <div class="movie-year"></div>
+    //         </div>
+    //         <p class="movie-overview">
 
-export const createMoviesMarkup = (movies) => {
-    if (!movies?.length) return false;
-
-    return movies.map(({ poster_path, original_title, original_name, release_date, overview }) => (
-        `
-        <li class="movie-item">
-            <img
-                class="movie-poster"
-                src="https://image.tmdb.org/t/p/w300/${poster_path}"
-                alt="${original_title || original_name}"
-                loading="lazy"
-            />
-            <div class="movie-info">
-                <div>
-                    <h3 class="movie-title">${original_title || original_name}</h3>
-                    <div class="movie-year">${release_date ? release_date : '-'}</div>
-                </div>
-                <p class="movie-overview">
-                   ${overview}
-                </p>
-            </div>
-        </li>
-        `
-    )).join('')
+    //         </p>
+    //     </div>
+    // </li>
 };
-
-export const showLoader = (active) => {
-    refs.loader.classList[active ? 'add' : 'remove']('active');
-}
-export const showResetBtn = (active) => {
-    refs.resetBtn.classList[active ? 'add' : 'remove']('active');
-}
