9378f8ddc686c210a1448ad939ec537dc8b6e8a1
NikolayMiroshnichenkoUK <myroshnychenko.mykola@flawless.group>
2025-06-13 17:55:37 +0300

    fix

---
+++ src/task-1.ts
@@ -0,0 +1,20 @@
+/*
+Завдання:
+    1. Перепиши цей код у TypeScript.
+    2. Створи тип для користувача.
+    3. Типізуй функцію getUserNames, щоб вона:
+        3.1 приймала масив об'єктів з правильним типом,
+        3.1 повертала масив рядків.
+*/
+
+// function getUserNames(users) {
+//   return users.map(user => user.name);
+// }
+
+// const users = [
+//   { id: 1, name: 'Ivan', age: 25 },
+//   { id: 2, name: 'Olha', age: 30 },
+//   { id: 3, name: 'Petro', age: 22 }
+// ];
+
+// console.log(getUserNames(users)); // ['Ivan', 'Olha', 'Petro'] 
