9d8376229356047657de84d508e5e069c0740ad1
NikolayMiroshnichenkoUK <myroshnychenko.mykola@flawless.group>
2025-06-21 14:30:08 +0300

    fix

--- src/main.ts
+++ src/main.ts
@@ -2,30 +2,43 @@
     Перепиши код з JS у TS
 */
 
-// function getUserNames(users) {
-//   return users.map(user => user.name);
+// type User = {
+//     id: number,
+//     name: string,
+//     age: number
+// }
+
+// function getUserNames(users: User[]): string[] {
+//     return users.map(user => user.name);
 // }
 
 // const users = [
-//   { id: 1, name: 'Ivan', age: 25 },
-//   { id: 2, name: 'Olha', age: 30 },
-//   { id: 3, name: 'Petro', age: 22 }
+//     { id: 1, name: 'Ivan', age: 25 },
+//     { id: 2, name: 'Olha', age: 30 },
+//     { id: 3, name: 'Petro', age: 22 }
 // ];
 
 // console.log(getUserNames(users)); // ['Ivan', 'Olha', 'Petro']
 
 // ==================================
 
-// function formatPrice(price, currency) {
-//     return `${price.toFixed(2)} ${currency || 'USD'}`;
+// function formatPrice(price: number, currency: string = 'USD'): string {
+//     return `${price.toFixed(2)} ${currency}`;
 // }
 
-// formatPrice(100);
-// formatPrice(50.5, 'EUR');
+// console.log(formatPrice(100));
+// console.log(formatPrice(50.5, 'EUR'));
 
 // ==================================
 
-// function logMessage(message) {
+// type MessageType = 'success' | 'error' | 'info' | 'warning';
+
+// type Message = {
+//     text: string,
+//     type: MessageType
+// }
+
+// function logMessage(message: Message): void {
 //   if (message.type === 'success') {
 //     console.log('✅ ' + message.text);
 //   } else if (message.type === 'error') {
@@ -43,27 +56,35 @@
 
 // ==================================
 
-// function handleOrderStatus(status) {
-//   if (status === 'pending') {
-//     console.log('Замовлення очікує обробки');
-//   } else if (status === 'shipped') {
-//     console.log('Замовлення відправлено');
-//   } else if (status === 'delivered') {
-//     console.log('Замовлення доставлено');
-//   } else {
-//     console.log('Невідомий статус');
-//   }
+// type Status = 'canceled' | 'shipped' | 'pending' | 'delivered' | string;
+
+// function handleOrderStatus(status: Status): void {
+//     if (status === 'pending') {
+//         console.log('Замовлення очікує обробки');
+//     } else if (status === 'shipped') {
+//         console.log('Замовлення відправлено');
+//     } else if (status === 'delivered') {
+//         console.log('Замовлення доставлено');
+//     } else if (status === 'canceled') {
+//         console.log('Замовлення відмінено');
+//     } else {
+//         console.log('Невідомий статус');
+//     }
 // }
 
 // handleOrderStatus('shipped');
 // handleOrderStatus('canceled');
+// handleOrderStatus('done');
+
 
 // ==================================
 
-// function printPoint(point) {
-//   const x = point[0];
-//   const y = point[1];
-//   console.log(`X: ${x}, Y: ${y}`);
+// type Points = [number, number];
+
+// function printPoint(point: Points): void {
+//     const x = point[0];
+//     const y = point[1];
+//     console.log(`X: ${x}, Y: ${y}`);
 // }
 
 // printPoint([10, 20]);
