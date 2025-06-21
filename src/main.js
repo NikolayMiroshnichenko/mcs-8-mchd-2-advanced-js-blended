678c4e49eb53d363d29d978cf66ec79e3f56651c
NikolayMiroshnichenkoUK <myroshnychenko.mykola@flawless.group>
2025-06-21 12:44:13 +0300

    fix

--- src/main.js
+++ src/main.js
@@ -1,9 +1,6 @@
 import { refs } from "./js/refs";
-import { resetMoviesList, showLoader, showLoadMoreBtn, createMoviesMarkup, showResetBtn } from './js/render-functions';
+import { resetMoviesList, showLoader, createMoviesMarkup, showResetBtn } from './js/render-functions';
 import { getTrendingMovies, getSearchMovies } from "./js/movies-api";
-
-let currentPage = 1;
-let isTrending = true;
 
 async function init() {
     resetMoviesList();
@@ -15,14 +12,9 @@ async function init() {
         const murkup = createMoviesMarkup(data?.results);
 
         showLoader(false);
-        
+
         refs.moviesList.insertAdjacentHTML('beforeend', murkup);
         refs.title.innerHTML = 'Найпопулярніші фільми за тиждень';
-
-        if (data.page < data.total_pages) {
-            currentPage = data.page += 1;
-            showLoadMoreBtn(true);
-        }
     } else {
         showLoader(false);
         refs.title.innerHTML = 'Поки що найпопулярніші фільми не доступні';
@@ -62,31 +54,6 @@ refs.form.addEventListener('submit', async (e) => {
 });
 
 refs.resetBtn.addEventListener('click', init);
-
-refs.loadMoreBtn.addEventListener('click', async () => {
-    showLoader(true);
-    showLoadMoreBtn(false);
-
-    if (isTrending) {
-        const data = await getTrendingMovies(currentPage);
-
-        if (data?.results?.length > 0) {
-            const murkup = createMoviesMarkup(data?.results);
-
-            showLoader(false);
-            refs.moviesList.insertAdjacentHTML('beforeend', murkup);
-
-            if (data.page < data.total_pages) {
-                currentPage = data.page += 1;
-                showLoadMoreBtn(true);
-            }
-        } else {
-            showLoader(false);
-        };
-    } else {
-
-    }
-});
 
 init();
 
